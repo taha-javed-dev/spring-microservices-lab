e79165515b07462fa8b1d9fd8e54ae506e28b998
taha-javed-dev <taha.javed.dev@gmail.com>
2026-05-30 13:07:49 +0530

    feat: add Eureka discovery service

--- order-service/src/main/java/com/project/ecommerce/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/project/ecommerce/order_service/controller/OrdersController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/core")
 @RequiredArgsConstructor
 @Slf4j
 public class OrdersController {
